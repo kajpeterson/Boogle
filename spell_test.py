9edd0a54756d6fbe353644682506dd382d673262
kajpeterson <61768919+kajpeterson@users.noreply.github.com>
2021-10-26 18:22:37 -0700

    Update spell_test.py

--- spell_test.py
+++ spell_test.py
@@ -1,7 +1,7 @@
 import enchant
 def spellcheck(word):
-    d = enchant.Dict("en_US")
-    if d.check(word):
+    pwl = enchant.request_pwl_dict("en")
+    if pwl.check(word):
       print(f"{word} is a real word.")
     else:
       print(f"{word} is not in the dictionary.")
