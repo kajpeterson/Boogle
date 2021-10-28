2e45bfe45be07f12e55d2c7c649247ac08b65e41
kajpeterson <61768919+kajpeterson@users.noreply.github.com>
2021-10-27 18:07:00 -0700

    Delete spell_test.py

--- spell_test.py
+++
@@ -1,8 +0,0 @@
-import enchant
-def spellcheck(word):
-    pwl = enchant.request_pwl_dict("en")
-    if pwl.check(word):
-      print(f"{word} is a real word.")
-    else:
-      print(f"{word} is not in the dictionary.")
-spellcheck("hello")
